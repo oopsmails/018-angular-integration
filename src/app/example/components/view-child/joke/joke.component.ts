3f9de75ebd757474136f290cf04ba257141a7028
oopsmails <oopsmails@gmail.com>
2022-01-12 23:29:59 -0500

    20220112: joke, shadow

--- src/app/example/components/view-child/joke/joke.component.ts
+++ src/app/example/components/view-child/joke/joke.component.ts
@@ -3,7 +3,8 @@ import { Joke } from '@app/shared/model/joke';
 
 @Component({
   selector: 'joke',
-  templateUrl: './joke.component.html'
+  templateUrl: './joke.component.html',
+  styleUrls: ['./joke.component.scss']
 })
 export class JokeComponent {
   @Input('joke') data: Joke;
