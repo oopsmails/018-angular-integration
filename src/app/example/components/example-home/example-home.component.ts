23bb8c1ad232790ae3d64be043299885c2929c75
oopsmails <oopsmails@gmail.com>
2022-01-12 22:23:50 -0500

    20220112: scss revising around

--- src/app/example/components/example-home/example-home.component.ts
+++ src/app/example/components/example-home/example-home.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-example-home',
-  templateUrl: './example-home.component.html'
+  templateUrl: './example-home.component.html',
+  styleUrls: ['./example-home.component.scss']
 })
 export class ExampleHomeComponent implements OnInit {
   title = 'app';
